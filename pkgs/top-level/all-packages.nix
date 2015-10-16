6509ba5f1666af4ef92309c22f64580287b0123b
Juho Östman <juho.ostman@kapsi.fi>
2015-10-16 14:33:08 +0300

    bcprov: init at 1.53

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6066,6 +6066,8 @@ let
 
   babl = callPackage ../development/libraries/babl { };
 
+  bcprov = callPackage ../development/libraries/java/bcprov { };
+
   beecrypt = callPackage ../development/libraries/beecrypt { };
 
   belle-sip = callPackage ../development/libraries/belle-sip { };
